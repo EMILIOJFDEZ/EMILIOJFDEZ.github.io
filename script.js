cd56616e517a4b02372f51d49ae85193fd63f418
EmilioJFdez <99140001+EMILIOJFDEZ@users.noreply.github.com>
2024-09-24 16:30:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,45 +2,40 @@ let menuVisible = false;
 //Función que oculta o muestra el menu
 function mostrarOcultarMenu(){
     if(menuVisible){
-        document.getElementById("nav").classList ="";
+        document.getElementById("nav").classList.remove("responsive");
         menuVisible = false;
     }else{
-        document.getElementById("nav").classList ="responsive";
+        document.getElementById("nav").classList.add("responsive");
         menuVisible = true;
     }
 }
-function SacarRatota()
-{
-    var Rata=document.getElementById("Ratota");
-    Rata.style.display="block";
-}
+
 function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
-    document.getElementById("nav").classList = "";
+    //oculto el menu una vez que selecciono una opción
+    document.getElementById("nav").classList.remove("responsive");
     menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
+
+// Función que aplica las animaciones de las habilidades
 function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[0].style.width = "60%";
+        habilidades[1].style.width = "80%";
+        habilidades[2].style.width = "40%";
+        habilidades[3].style.width = "90%";
+        habilidades[4].style.width = "60%";
+        habilidades[5].style.width = "80%";
+        habilidades[6].style.width = "70%";
+        habilidades[7].style.width = "100%";
+        habilidades[8].style.width = "75%";
+        habilidades[9].style.width = "80%";
     }
 }
 
-
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
+// Detecto el scrolling para aplicar la animación de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
 }
